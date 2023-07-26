27b113b2505d4ac34535719fb95187dd4268de62
성호  권 <maystar0503@naver.com>
2023-07-27 04:58:46 +0900

    [add] Topbar 컴포넌트 구조 잡기

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./topbar.css";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import LanguageIcon from "@mui/icons-material/Language";
+import SettingsIcon from "@mui/icons-material/Settings";
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">Admin</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNoneIcon />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <LanguageIcon />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <SettingsIcon />
+            <span className="topIconBadge">2</span>
+          </div>
+          <img src=" " alt="" className="topAvatar" />
+        </div>
+      </div>
+    </div>
+  );
+}

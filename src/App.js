7705216694de2ef24597a4558097cf058bcf206b
성호  권 <maystar0503@naver.com>
2023-07-27 06:15:30 +0900

    [추가] sidebar 컴포넌트 구조 잡기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import Topbar from "./components/topbar/Topbar";
+import Sidebar from "./components/sidebar/sidebar";
 
 function App() {
   return (
     <div className="App">
       <Topbar />
+      <div className="container">
+        <Sidebar />
+        <div className="others">other pages</div>
+      </div>
     </div>
   );
 }
---
+++ src/components/sidebar/sidebar.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import "./sidebar.css";
+import HomeWorkIcon from "@mui/icons-material/HomeWork";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+
+export default function sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem active">
+              <HomeWorkIcon />
+              Home
+            </li>
+            <li className="sidebarListItem">
+              <TrendingUpIcon />
+              Analystics
+            </li>
+            <li className="sidebarListItem">
+              <AttachMoneyIcon />
+              Sales
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <HomeWorkIcon />
+              Home
+            </li>
+            <li className="sidebarListItem">
+              <TrendingUpIcon />
+              Analystics
+            </li>
+            <li className="sidebarListItem">
+              <AttachMoneyIcon />
+              Sales
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <HomeWorkIcon />
+              Home
+            </li>
+            <li className="sidebarListItem">
+              <TrendingUpIcon />
+              Analystics
+            </li>
+            <li className="sidebarListItem">
+              <AttachMoneyIcon />
+              Sales
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
